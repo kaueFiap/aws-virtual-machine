35ac5463701a368a91a78152537cf96aae62304a
Kledson Basso <kledsonhugo@gmail.com>
2025-09-10 14:38:16 -0300

    Atualiza a versão do Terraform para 1.13.1 e do provedor AWS para 6.11.0; adiciona arquivo de pipeline do GitHub Actions para automação do Terraform.

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,20 +1,20 @@
 # PROVIDER
 terraform {
 
-  required_version = "~> 1.12.2"
+  required_version = "1.13.1"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.9.0"
+      version = "~> 6.11.0"
     }
   }
 
-}
+  backend "s3" {
+    bucket       = "aws-vm-tfstate-kledsonbasso"
+    key          = "tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
 
-provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = [".aws/config"]
-  shared_credentials_files = [".aws/credentials"]
-  profile                  = "iac"
 }
