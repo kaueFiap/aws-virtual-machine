ea4a931a4ad2620d8f858fac8f6902a3a19c9957
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-17 20:58:09 -0300

    meu primeiro commit para testes

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -5,18 +5,38 @@ resource "aws_vpc" "this" {
   tags                 = merge(var.tags, { Name = "vm-vpc" })
 }
 
+# Flow Logs para a VPC
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name              = "/aws/vpc/flow-logs"
+  retention_in_days = 7
+}
+
+resource "aws_flow_log" "vpc" {
+  log_destination      = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.this.id
+  log_destination_type = "cloud-watch-logs"
+}
+
+# Restringe o SG default da VPC
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.this.id
+  ingress = []
+  egress  = []
+}
+
 # IGW
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
   tags   = merge(var.tags, { Name = "vm-igw" })
 }
 
-# Subnet pública
+# Subnet pública (sem IP público automático)
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.public_subnet_cidr
   availability_zone       = var.az
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
   tags                    = merge(var.tags, { Name = "vm-public" })
 }
 
@@ -35,39 +55,55 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public.id
 }
 
-# SG
+# Security Group
 resource "aws_security_group" "public" {
-  name   = "sg_public"
-  vpc_id = aws_vpc.this.id
+  name        = "sg_public"
+  description = "Security group para tráfego público"
+  vpc_id      = aws_vpc.this.id
 
+  # Egress seguro apenas para portas necessárias (ex.: HTTP e HTTPS)
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    description = "Saída HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # tráfego interno (seu /16)
+  egress {
+    description = "Saída HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Tráfego interno
   ingress {
+    description = "Tráfego interno permitido"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = [var.allow_intra_cidr]
   }
 
-  # ssh e http públicos
+  # SSH
   dynamic "ingress" {
     for_each = var.allow_ssh_cidr
     content {
+      description = "Acesso SSH"
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
       cidr_blocks = [ingress.value]
     }
   }
+
+  # HTTP
   dynamic "ingress" {
     for_each = var.allow_http_cidr
     content {
+      description = "Acesso HTTP"
       from_port   = 80
       to_port     = 80
       protocol    = "tcp"
