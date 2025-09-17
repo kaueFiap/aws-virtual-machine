ebed1988127182593cb7ec6755b80ff65ba3278f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-17 20:54:48 -0300

    meu primeiro commit para testes

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -5,15 +5,15 @@ resource "aws_instance" "this" {
   vpc_security_group_ids = var.security_group_ids
   key_name               = var.key_name
   user_data_base64       = base64encode(templatefile(var.user_data_path, {}))
-  monitoring             = true        # habilita métricas
-  ebs_optimized          = true        # EBS otimizado
+  monitoring             = true # habilita métricas
+  ebs_optimized          = true # EBS otimizado
 
   metadata_options {
-    http_tokens = "required"           # IMDSv2
+    http_tokens = "required" # IMDSv2
   }
 
   root_block_device {
-    encrypted = true                   # Criptografa o disco
+    encrypted = true # Criptografa o disco
   }
 
   tags = merge(var.tags, { Name = var.name })
